84feb63091db29917784415257dc65db4884eac7
Brenda <brendakungu04@gmail.com>
2024-01-09 12:18:56 +0300

    flask prac

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(response):
+    print(f'This web server is running at {request.remote_addr}')
+    return('A WSGI generated this response! and i love how i am flowing. Thankyou God')
+    
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
+
